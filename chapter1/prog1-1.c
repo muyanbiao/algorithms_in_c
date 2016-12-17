0f2946ba3906b35630501f7a034e5808a4cbd4cf
Michael Mu <muyanbiao@gmail.com>
2016-12-17 10:28:00 +0800

    联通问题的快速查找算法

--- chapter1/prog1-1.c
+++ chapter1/prog1-1.c
@@ -9,10 +9,12 @@ int main()
 	for (int i = 0; i < N; i++) id[i] = i;
 
 	while(scanf("%d %d\n", &p, &q) == 2){
-		if (id[p] == id[q]) continue;
+		if (id[p] == id[q]) continue; // 查找操作，只需要比较数组中对应位置上的元素是否相等
+		// 合并操作，每次都要扫描整个数组
 		for (t = id[p], i = 0; i < N; i++)
 			if(id[i] == t) id[i] = id[q];
-		printf("%d %d\n", p, q);
+
+		printf("%d %d\n", p, q); // 打印不在集合中的数对
 	}
 	return 0;
 }
